9b3b4b80af1ea13444bd91fa3888ba664092011c
Marc Belmont <marc@marc-hp.(none)>
2012-10-19 14:55:10 +1100

    #513 fix discount calculation when the discount is not specific

--- cartridge/shop/models.py
+++ cartridge/shop/models.py
@@ -890,8 +890,7 @@ class Cart(models.Model):
         """
         # Discount applies to cart total if not product specific.
         products = discount.all_products()
-        if products.count() == 0:
-            return discount.calculate(self.total_price(), currency)
+        specific_products = len(products[:1]) > 0
         total = Decimal("0")
         # Create a list of skus in the cart that are applicable to
         # the discount, and total the discount for appllicable items.
@@ -901,7 +900,11 @@ class Cart(models.Model):
         from multicurrency.models import MultiCurrencyProductVariation
         for item in self:
             mc_variation = MultiCurrencyProductVariation.objects.get(sku=item.sku)
-            if item.sku in discount_skus and not (mc_variation.on_sale(currency) or mc_variation.is_marked_down(currency)):
+            if (mc_variation.on_sale(currency) or
+                mc_variation.is_marked_down(currency)):
+                continue
+            if ((specific_products and item.sku in discount_skus) or
+                not specific_products):
                 total += discount.calculate(item.unit_price, currency) * item.quantity
         return total
 
