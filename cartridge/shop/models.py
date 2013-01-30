1e3cfeaf8f02247d154c941a718851983208d3c6
DamianHeard <damian@commoncode.com>
2013-01-30 16:06:54 +1100

    #COT-74 the related products are no longer symmetric.

--- cartridge/shop/models.py
+++ cartridge/shop/models.py
@@ -150,7 +150,7 @@ class Product(Displayable, Priced, RichText):
         _("Stock Last Synced"), null=True,
         help_text="When stock for this product was last synced from RMS."
     )
-    related_products = models.ManyToManyField("self", blank=True)
+    related_products = models.ManyToManyField("self", blank=True, symmetrical=False)
     upsell_products = models.ManyToManyField("self", blank=True)
     rating = RatingField(verbose_name=_("Rating"))
     featured = models.BooleanField(_("Featured Product"), default=False)
