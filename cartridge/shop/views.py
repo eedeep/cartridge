60a6b5c3259efbd052a267b41d6be5ae0e45e70f
Marc Belmont <marc.belmont@cottonon.com.au>
2013-07-08 14:45:20 +1000

    update GPP points logic to match the codes provided cot-685

--- cartridge/shop/views.py
+++ cartridge/shop/views.py
@@ -744,7 +744,7 @@ def complete(request, template="shop/complete.html", extends_template="base.html
     discount = get_or_create_discount(order)
     gpp_code = GPPPoint.gpp_code(order)
     context = {"order": order,
-               'gpp_code': gpp_code,
+               'gpp_code': gpp_code and order.id != request.session.get('latest_order', ''),
                "items": items,
                'track_transaction': order.id != request.session.get('latest_order', ''),
                "extends_template": extends_template,
